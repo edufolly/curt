d7452a5b1c6b4040329da510b51e2ac0a1ebf430
Eduardo Folly <edufolly@gmail.com>
2023-06-20 15:34:55 -0300

    Updating tests.

--- test/curt_test.dart
+++ test/curt_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: format-comment
+
 import 'dart:io';
 import 'dart:math';
 
@@ -12,109 +14,152 @@ import '../helpers/basic_test_result.dart';
 ///
 void main() {
   ///
-  group('Basic Tests', () {
-    final Curt curt = Curt();
+  group(
+    'Basic Tests',
+    () {
+      final Curt curt = Curt();
 
-    Map<String, BasicTestResult> tests = <String, BasicTestResult>{
-      'https://httpbin.org/status/200': BasicTestResult(
-        statusCode: 200,
-        headersMatcher: isNotEmpty,
-        bodyMatcher: isEmpty,
-      ),
-      'https://httpbin.org/status/403': BasicTestResult(
-        statusCode: 403,
-        headersMatcher: isNotEmpty,
-        bodyMatcher: isEmpty,
-      ),
-      'https://httpbin.org/status/404': BasicTestResult(
-        statusCode: 404,
-        headersMatcher: isNotEmpty,
-        bodyMatcher: isEmpty,
-      ),
-      'https://httpbin.org/status/500': BasicTestResult(
-        statusCode: 500,
-        headersMatcher: isNotEmpty,
-        bodyMatcher: isEmpty,
-      ),
-    };
+      const String scheme = 'http';
+      const String server = '127.0.0.1';
+      final int httpPort = Random().nextInt(55535) + 10000;
+      const Duration timeLimit = Duration(seconds: 60);
 
-    for (final MapEntry<String, BasicTestResult> entry in tests.entries) {
-      test('GET ${entry.key}', () async {
-        Response response = await curt.get(Uri.parse(entry.key));
-        expect(response.statusCode, entry.value.statusCode);
-        expect(response.headers, entry.value.headersMatcher);
-        expect(response.body, entry.value.bodyMatcher);
+      const String containerImage = 'kennethreitz/httpbin';
+      final String containerName = 'server${DateTime.now().millisecond}';
+
+      ///
+      setUpAll(() async {
+        ProcessResult result = await Process.run('docker', <String>[
+          '--version',
+        ]).timeout(timeLimit);
+
+        expect(result.exitCode, 0, reason: result.stderr);
+
+        result = await Process.run('docker', <String>[
+          'run',
+          '--rm',
+          '--name',
+          containerName,
+          '-p',
+          '$httpPort:80',
+          '-d',
+          containerImage,
+        ]).timeout(timeLimit);
+
+        expect(result.exitCode, 0, reason: result.stderr);
+
+        /// Time to container starts.
+        await Future<void>.delayed(Duration(seconds: 10));
       });
 
-      test('POST ${entry.key}', () async {
-        Response response = await curt.post(Uri.parse(entry.key));
-        expect(response.statusCode, entry.value.statusCode);
-        expect(response.headers, entry.value.headersMatcher);
-        expect(response.body, entry.value.bodyMatcher);
-      });
+      Map<String, BasicTestResult> tests = <String, BasicTestResult>{
+        '$scheme://$server:$httpPort/status/200': BasicTestResult(
+          statusCode: 200,
+          headersMatcher: isNotEmpty,
+          bodyMatcher: isEmpty,
+        ),
+        '$scheme://$server:$httpPort/status/403': BasicTestResult(
+          statusCode: 403,
+          headersMatcher: isNotEmpty,
+          bodyMatcher: isEmpty,
+        ),
+        '$scheme://$server:$httpPort/status/404': BasicTestResult(
+          statusCode: 404,
+          headersMatcher: isNotEmpty,
+          bodyMatcher: isEmpty,
+        ),
+        '$scheme://$server:$httpPort/status/500': BasicTestResult(
+          statusCode: 500,
+          headersMatcher: isNotEmpty,
+          bodyMatcher: isEmpty,
+        ),
+      };
 
-      test('PUT ${entry.key}', () async {
-        Response response = await curt.put(Uri.parse(entry.key));
-        expect(response.statusCode, entry.value.statusCode);
-        expect(response.headers, entry.value.headersMatcher);
-        expect(response.body, entry.value.bodyMatcher);
-      });
+      for (final MapEntry<String, BasicTestResult> entry in tests.entries) {
+        test('GET ${entry.key}', () async {
+          Response response = await curt.get(Uri.parse(entry.key));
+          expect(response.statusCode, entry.value.statusCode);
+          expect(response.headers, entry.value.headersMatcher);
+          expect(response.body, entry.value.bodyMatcher);
+        });
 
-      test('DELETE ${entry.key}', () async {
-        Response response = await curt.delete(Uri.parse(entry.key));
-        expect(response.statusCode, entry.value.statusCode);
-        expect(response.headers, entry.value.headersMatcher);
-        expect(response.body, entry.value.bodyMatcher);
-      });
-    }
-  });
+        test('POST ${entry.key}', () async {
+          Response response = await curt.post(Uri.parse(entry.key));
+          expect(response.statusCode, entry.value.statusCode);
+          expect(response.headers, entry.value.headersMatcher);
+          expect(response.body, entry.value.bodyMatcher);
+        });
 
-  ///
-  group('Redirect Tests', () {
-    final Curt curt = Curt(followRedirects: true);
+        test('PUT ${entry.key}', () async {
+          Response response = await curt.put(Uri.parse(entry.key));
+          expect(response.statusCode, entry.value.statusCode);
+          expect(response.headers, entry.value.headersMatcher);
+          expect(response.body, entry.value.bodyMatcher);
+        });
 
-    test('Redirect', () async {
-      Uri uri = Uri(
-        scheme: 'https',
-        host: 'httpbin.org',
-        path: 'redirect-to',
-        queryParameters: <String, dynamic>{
-          'url': 'https://httpbin.org/status/200',
-        },
-      );
+        test('DELETE ${entry.key}', () async {
+          Response response = await curt.delete(Uri.parse(entry.key));
+          expect(response.statusCode, entry.value.statusCode);
+          expect(response.headers, entry.value.headersMatcher);
+          expect(response.body, entry.value.bodyMatcher);
+        });
+      }
 
-      Response response = await curt.get(uri);
+      for (int gen = 0; gen < 3; gen++) {
+        int bytes = Random().nextInt(1024);
+        test('Body Length $bytes', () async {
+          Response response = await curt.get(
+            Uri.parse('$scheme://$server:$httpPort/range/$bytes'),
+          );
 
-      expect(response.statusCode, 200);
-      expect(response.headers, isNotEmpty);
-      expect(response.headers.containsKey('location'), isFalse);
-      expect(response.body, isEmpty);
-    });
-  });
+          expect(response.statusCode, 200);
+          expect(response.headers, isNotEmpty);
+          expect(response.headers.containsKey('Content-Length'), isTrue);
+          expect(
+            int.tryParse(response.headers['Content-Length'].toString()) ?? -1,
+            bytes,
+          );
+          expect(response.body, isNotEmpty);
+          expect(response.body.length, bytes);
+        });
+      }
 
-  ///
-  group('Body Size Tests', () {
-    final Curt curt = Curt();
+      test('Redirect', () async {
+        final Curt curt = Curt(followRedirects: true);
 
-    for (int gen = 0; gen < 3; gen++) {
-      int bytes = Random().nextInt(1024);
-      test('Body Length $bytes', () async {
-        Response response = await curt.get(
-          Uri.parse('http://httpbin.org/range/$bytes'),
+        Uri uri = Uri(
+          scheme: scheme,
+          host: server,
+          port: httpPort,
+          path: 'redirect-to',
+          queryParameters: <String, dynamic>{
+            'url': '$scheme://$server:$httpPort/status/200',
+          },
         );
+
+        Response response = await curt.get(uri);
 
         expect(response.statusCode, 200);
         expect(response.headers, isNotEmpty);
-        expect(response.headers.containsKey('Content-Length'), isTrue);
-        expect(
-          int.tryParse(response.headers['Content-Length'].toString()) ?? -1,
-          bytes,
-        );
-        expect(response.body, isNotEmpty);
-        expect(response.body.length, bytes);
+        expect(response.headers.containsKey('location'), isFalse);
+        expect(response.body, isEmpty);
       });
-    }
-  });
+
+      ///
+      tearDownAll(() async {
+        ProcessResult result = await Process.run('docker', <String>[
+          'stop',
+          containerName,
+        ]).timeout(timeLimit);
+
+        expect(result.exitCode, 0, reason: result.stderr);
+      });
+    },
+    onPlatform: <String, dynamic>{
+      'mac-os': Skip('No docker installed on GitHub actions.'),
+      'windows': Skip('Need a windows container image.'),
+    },
+  );
 
   ///
   group(
@@ -124,7 +169,7 @@ void main() {
       const String server = '127.0.0.1';
       const int httpPort = 8080;
       const int httpsPort = 8443;
-      const Duration timeLimit = Duration(seconds: 120);
+      const Duration timeLimit = Duration(seconds: 60);
 
       const String containerImage = 'mendhak/http-https-echo:29';
       final String containerName = 'server${DateTime.now().millisecond}';
